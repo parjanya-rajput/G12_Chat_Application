b6ef9dd32440ac58a78729d0a6e0c693a7120bd7
DevDodiya <dev.m.dodiya@gmail.com>
2024-11-07 23:49:39 +0530

    Created OnBoarding Screen

--- ChatApp/src/components/atoms/ReusableButton/index.js
+++ ChatApp/src/components/atoms/ReusableButton/index.js
@@ -1,16 +1,37 @@
 // index.js of Reusable Button
 
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text,  Dimensions } from 'react-native';
 import styles from './style';
+import { useState, useEffect } from 'react';
+import {ActivityIndicator } from 'react-native';
+import * as Font from 'expo-font';
 
-const ReusableButton = ({ text, backgroundColor, textColor, onPress }) => {
+const ReusableButton = ({ text, backgroundColor, textColor, onPress, topval }) => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'Caros-Heavy': require('../../../../assets/fonts/cretype  Caros Heavy.otf'),
+        'Caros-Medium': require('../../../../assets/fonts/cretype  Caros Medium.otf'),
+        'Caros-Light': require('../../../../assets/fonts/cretype  Caros Light Italic.otf'),
+        // ../assets/fonts/cretype Caros Heavy.otf
+      });
+      setFontsLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" color="#FFFFFF" />;
+  }
   return (
     <TouchableOpacity
-      style={[styles.button, { backgroundColor: backgroundColor }]}
+      style={[styles.button, { backgroundColor: backgroundColor }, {top:topval}]}
       onPress={onPress}
     >
-      <Text style={[styles.buttonText]}>
+      <Text style={[styles.buttonText, {color:textColor}]}>
         {text}
       </Text>
     </TouchableOpacity>
--- ChatApp/src/screens/HomeScreen.js
+++ ChatApp/src/screens/HomeScreen.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-
 const HomeScreen = ({ navigation }) => {
     return (
         <View>
             <Text>Home Screen</Text>
             <Button title="Open Drawer" onPress={() => navigation.openDrawer()} />
             <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
-
+            <Button title="Go to OnBoardingScreen" onPress={() => navigation.navigate('OnBoardingScreen')} />
         </View>
     );
 };

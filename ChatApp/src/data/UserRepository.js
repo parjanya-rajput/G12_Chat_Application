6d040b4e2d3dff0d54a367201b0764288c2bcd58
akhaja-D6255 <akhajadhruvin@gmail.com>
2024-11-13 17:11:36 +0530

    Implemented Backend Search functionality and onlclick fetch perticular userDetails from FireBase

--- ChatApp/src/data/UserRepository.js
+++ ChatApp/src/data/UserRepository.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs , query, where } from 'firebase/firestore';
 import { firestore } from '../firebase/firebase';
 
 class UserRepository {
@@ -8,6 +8,27 @@ class UserRepository {
             return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         } catch (error) {
             console.error("Error fetching users:", error);
+            throw error;
+        }
+    }
+
+    async getUserByUsername(user_name) {
+        try {
+            // Create a query to find the user with the specified user_name
+            const q = query(collection(firestore, "users"), where("user_name", "==", user_name));
+            const querySnapshot = await getDocs(q);
+
+            if (querySnapshot.empty) {
+                console.log("No user found with user_name:", user_name);
+                return null;
+            }
+
+            // Assuming user_name is unique and should return only one result
+            const userData = querySnapshot.docs[0].data();
+            // console.log("User details:", userData);
+            return userData;
+        } catch (error) {
+            console.error("Error fetching user by user_name:", error);
             throw error;
         }
     }

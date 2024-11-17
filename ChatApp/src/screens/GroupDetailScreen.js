e223a2a9bfeff42048cfb24528d0a52d470bc396
Sahil Sutariya <202201119@daiict.ac.in>
2024-11-17 16:16:39 +0530

    created GroupDetailsScreen and AddMemberScreen and some essential files required for that

---
+++ ChatApp/src/screens/GroupDetailScreen.js
@@ -0,0 +1,9 @@
+// screens/GroupDetailsScreen.js
+import React from 'react';
+import GroupDetails from '../components/organisms/groupDetails';
+
+const GroupDetailsScreen = ({ route }) => {
+    return <GroupDetails route={route} />;
+};
+
+export default GroupDetailsScreen;

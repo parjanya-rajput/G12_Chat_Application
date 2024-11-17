b9d998512f038307ecc4f0709d668871f36466d9
Sahil Sutariya <202201119@daiict.ac.in>
2024-11-17 16:24:02 +0530

    Revert "created GroupDetailsScreen and AddMemberScreen and some essential files required for that"
    This reverts commit e223a2a9bfeff42048cfb24528d0a52d470bc396.

--- ChatApp/src/screens/GroupDetailScreen.js
+++
@@ -1,9 +0,0 @@
-// screens/GroupDetailsScreen.js
-import React from 'react';
-import GroupDetails from '../components/organisms/groupDetails';
-
-const GroupDetailsScreen = ({ route }) => {
-    return <GroupDetails route={route} />;
-};
-
-export default GroupDetailsScreen;
